4dd25770eb586696f59faa291a85590ae86342bb
alabester <37791766+alabester@users.noreply.github.com>
2018-06-09 14:29:05 +0900

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,324 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.UIManager;
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JTextField;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.LinearGradientPaint;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Dimension;
+import java.awt.Insets;
+import javax.swing.JRadioButton;
+
+import java.sql.*;
+
+public class Login extends JFrame implements FocusListener {
+
+    public Connection con = null;
+    public Statement st = null;
+    public ResultSet rs = null;
+    
+    private PreparedStatement pstmtsqlInsert;
+    
+	private JPanel contentPane;
+	private JTextField id_1;
+	private JTextField pwd_1;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JPasswordField passwordField;
+	private boolean showingHint;
+	private String TextContent;
+	private JPasswordField passwordField_1;
+	private JTextField txtYyyy;
+	private JTextField txtMm;
+	private JTextField txtDd;
+	private JLabel lblNewLabel_3;
+	private JLabel lblNewLabel_2;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+
+					Login frame = new Login();
+					frame.setVisible(true);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		
+		DBConnection();
+		
+		lblNewLabel_2 = null;
+		lblNewLabel_3 = null;
+
+		setUndecorated(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(200, 200, 900, 800);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblPassword = new JLabel("비밀번호");
+		lblPassword.setBounds(630, 10, 90, 25);
+		lblPassword.setForeground(Color.WHITE);
+		lblPassword.setFont(new Font("다음_Regular", Font.PLAIN, 15));
+		contentPane.add(lblPassword);
+		
+		pwd_1 = new JPasswordField();
+		pwd_1.setBounds(630, 40, 150, 25);
+		pwd_1.setColumns(10);
+		contentPane.add(pwd_1);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 900, 90);
+		panel.setBackground(new Color(72,103,170));
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lbID = new JLabel("이메일, 또는 휴대폰");
+		lbID.setBounds(450, 10, 200, 25);
+		panel.add(lbID);
+		lbID.setForeground(Color.WHITE);
+		lbID.setFont(new Font("다음_Regular", Font.PLAIN, 15));
+		
+		id_1 = new JTextField();
+		id_1.setBounds(450, 40, 150, 25);
+		panel.add(id_1);
+		id_1.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("D:\\Downloads\\facebook_2015_logo_detail (1).png"));
+		lblNewLabel.setBounds(31, 10, 371, 69);
+		panel.add(lblNewLabel);
+		
+		JButton button = new JButton("로그인");
+		button.setBounds(797, 40, 59, 25);
+		panel.add(button);
+		button.setMargin(new Insets(0, 0, 0, 0));
+		button.setPreferredSize(new Dimension(59, 25));
+		button.setForeground(Color.WHITE);
+		button.setFont(new Font("다음_Regular", Font.PLAIN, 15));
+		button.setBackground(new Color(72, 103, 170));
+		
+		JLabel lblNewLabel_1 = new JLabel("가입하기");
+		lblNewLabel_1.setBounds(450, 144, 159, 54);
+		lblNewLabel_1.setForeground(new Color(0, 0, 0));
+		lblNewLabel_1.setFont(new Font("다음_Regular", Font.PLAIN, 38));
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel label = new JLabel("항상 지금처럼 무료로 즐기실 수 있습니다.");
+		label.setBounds(450, 210, 424, 34);
+		label.setForeground(Color.BLACK);
+		label.setFont(new Font("다음_Regular", Font.PLAIN, 22));
+		contentPane.add(label);
+		
+		textField = new JTextField();
+		textField.setText("성(姓)");
+		textField.setHorizontalAlignment(SwingConstants.LEFT);
+		textField.setToolTipText("");
+		textField.setBounds(450, 260, 170, 34);
+		textField.addFocusListener(this);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setText("이름");
+		textField_1.setColumns(10);
+		textField_1.setBounds(647, 260, 159, 34);
+		textField_1.addFocusListener(this);
+		contentPane.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setText("휴대폰 번호 또는 이메일");
+		textField_2.setColumns(10);
+		textField_2.setBounds(450, 306, 356, 34);
+		textField_2.addFocusListener(this);
+		contentPane.add(textField_2);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(450, 395, 356, 34);
+		passwordField.addFocusListener(this);
+		contentPane.add(passwordField);
+		
+		JLabel label_1 = new JLabel("비밀번호");
+		label_1.setForeground(Color.BLACK);
+		label_1.setFont(new Font("다음_Regular", Font.PLAIN, 18));
+		label_1.setBounds(450, 352, 424, 34);
+		contentPane.add(label_1);
+		
+		passwordField_1 = new JPasswordField();
+		passwordField_1.setBounds(450, 470, 356, 34);
+		contentPane.add(passwordField_1);
+		
+		JLabel label_2 = new JLabel("확인을 위해 다시 입력해 주세요.");
+		label_2.setForeground(Color.BLACK);
+		label_2.setFont(new Font("다음_Regular", Font.PLAIN, 18));
+		label_2.setBounds(450, 433, 424, 34);
+		contentPane.add(label_2);
+		
+		JButton btnNewButton = new JButton("가입하기");
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setBackground(new Color(72,103,170));
+		btnNewButton.setFont(new Font("다음_Regular", Font.PLAIN, 15));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(!textField.getText().equals("") && !passwordField.getText().equals("") && !textField_1.getText().equals("") && !textField_2.getText().equals("") && !txtYyyy.getText().equals("") && !txtMm.getText().equals("") && !txtDd.getText().equals("") && !passwordField_1.getText().equals(""))
+				{	lblNewLabel_2.setIcon(new ImageIcon("D:\\Downloads\\icons8-checked-40 (1).png"));
+					lblNewLabel_3.setForeground(Color.BLUE);
+					lblNewLabel_3.setText("성공적으로 입력되었습니다.");
+					InsertQuery();
+				}
+				else {
+				System.out.println(textField.getText());
+				lblNewLabel_3.setForeground(Color.RED);
+				lblNewLabel_2.setIcon(new ImageIcon("D:\\Downloads\\icons8-high-priority-48 (1).png"));
+				lblNewLabel_3.setText("필수항목이 제대로 입력되지 않았습니다.");
+			}
+			}
+		});
+		btnNewButton.setBounds(450, 694, 150, 40);
+		contentPane.add(btnNewButton);
+		
+		JLabel label_3 = new JLabel("생일");
+		label_3.setForeground(Color.BLACK);
+		label_3.setFont(new Font("다음_Regular", Font.PLAIN, 18));
+		label_3.setBounds(450, 519, 356, 34);
+		contentPane.add(label_3);
+		
+		txtYyyy = new JTextField();
+		txtYyyy.setToolTipText("");
+		txtYyyy.setText("YYYY");
+		txtYyyy.setHorizontalAlignment(SwingConstants.LEFT);
+		txtYyyy.setColumns(10);
+		txtYyyy.setBounds(450, 557, 97, 34);
+		txtYyyy.addFocusListener(this);
+		contentPane.add(txtYyyy);
+		
+		txtMm = new JTextField();
+		txtMm.setToolTipText("");
+		txtMm.setText("MM");
+		txtMm.setHorizontalAlignment(SwingConstants.LEFT);
+		txtMm.setColumns(10);
+		txtMm.setBounds(591, 557, 81, 34);
+		txtMm.addFocusListener(this);
+		contentPane.add(txtMm);
+		
+		txtDd = new JTextField();
+		txtDd.setToolTipText("");
+		txtDd.setText("DD");
+		txtDd.setHorizontalAlignment(SwingConstants.LEFT);
+		txtDd.setColumns(10);
+		txtDd.setBounds(725, 557, 81, 34);
+		txtDd.addFocusListener(this);
+		contentPane.add(txtDd);
+		
+		lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setIcon(null);
+		lblNewLabel_2.setBounds(450, 622, 54, 34);
+		contentPane.add(lblNewLabel_2);
+		
+		lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setFont(new Font("맑은 고딕", Font.PLAIN, 15));
+		lblNewLabel_3.setForeground(Color.RED);
+		lblNewLabel_3.setBounds(488, 617, 292, 40);
+		contentPane.add(lblNewLabel_3);
+		
+		
+        int panelHeight = contentPane.getHeight();
+        int panelWidth = contentPane.getWidth();
+        GradientPaint gradientPaint = new GradientPaint( panelWidth / 2 , 0 , new Color(0,0,0) , panelWidth / 2 , panelHeight , new Color(255,255,255) );
+
+	}
+	
+	  public void DBConnection() {
+		  try {
+		  Class.forName("com.mysql.jdbc.Driver");
+	        con = DriverManager.getConnection("jdbc:mysql://35.166.92.90:3306/sampledb" , "sampledb_dba", "1234");
+		  } catch (Exception e) {
+			  e.printStackTrace();
+		  }
+	  }
+	  
+	  public void InsertQuery() {
+		  String SQL = "Insert into member values (?,?,?,?,?)";
+		 
+		  String birthdate_string = (txtYyyy.getText() + "-" + txtMm.getText() + "-" + txtDd.getText());
+		  
+		  Date birthdate = Date.valueOf(birthdate_string);
+		  try {
+		  pstmtsqlInsert = con.prepareStatement(SQL);
+		  
+		  pstmtsqlInsert.setString(1, textField_2.getText());
+		  pstmtsqlInsert.setString(2, passwordField_1.getText());
+		  pstmtsqlInsert.setString(3, textField.getText());
+		  pstmtsqlInsert.setString(4, textField_1.getText());
+		  pstmtsqlInsert.setDate(5, birthdate);
+		  
+		  pstmtsqlInsert.executeUpdate();
+		  System.out.println(pstmtsqlInsert);
+			
+		  } catch (Exception e) {
+			  e.printStackTrace();
+		  }
+
+	  }
+	  
+	  public void focusLost(FocusEvent e) {
+		  Component source = e.getComponent();
+		  if(((JTextField)source).getText().isEmpty()) {
+		  if(source instanceof JTextField) {
+			  ((JTextField)source).setText(TextContent);
+		  }
+			 }  
+	  }
+	  
+	  public void focusGained(FocusEvent e) {
+		  Component source = e.getComponent();
+		  if(source instanceof JTextField) {
+			  TextContent = ((JTextField)source).getText();
+			  ((JTextField)source).setText("");
+			  }  
+		  }
+	  
+	  public String getText(String JTextFieldHName) {
+		    return showingHint ? "" : JTextFieldHName;
+		  }
+}
---
+++ MainPage_TestBed.java
@@ -0,0 +1,310 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.MatteBorder;
+
+public class MainPage_TestBed extends JFrame {
+
+    public Connection con = null;
+    public PreparedStatement st = null;
+    public ResultSet rs = null;
+	ArrayList<String> contents = new ArrayList<>();
+	ArrayList<String> names = new ArrayList<>();
+	
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                 MainPage_TestBed mn = new MainPage_TestBed();
+            }
+        });
+    }
+	/**
+	 * Create the frame.
+	 */
+	public MainPage_TestBed() {
+
+        JFrame frame = new JFrame("SpringLayout");
+        frame.getContentPane().setBackground(new Color(233, 235, 238));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JScrollPane scroll = new JScrollPane();
+        scroll.setBounds(400, 100, 640, 900);
+        Container contentPane = frame.getContentPane();
+        SpringLayout layout = new SpringLayout();
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(layout);
+
+        DBConnection();
+        
+		int rowCnt = 0;
+		
+		try {
+
+			
+			String Count = "select count(*) from content";
+			st = con.prepareStatement(Count);
+			rs = st.executeQuery();
+				while(rs.next()) {
+					rowCnt = rs.getInt(1);
+				}
+			System.out.println(rowCnt);
+			
+			String SQL = "select * from content order by contentID desc";
+			st = con.prepareStatement(SQL);
+			rs = st.executeQuery();
+
+			Iterator it = contents.iterator();
+			
+			while(rs.next())
+				{
+				contents.add(rs.getString("content"));
+				names.add(rs.getString("userID"));
+				}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        
+        
+        int j = 300;
+        int initialHeight = 0;
+        int initialX = 0;
+        
+        
+        	JPanel Write = new JPanel();
+        	layout.putConstraint(SpringLayout.NORTH, Write, 0, SpringLayout.NORTH, mainPanel);
+        	layout.putConstraint(SpringLayout.WEST, Write, 0, SpringLayout.WEST, mainPanel);
+        	
+        	Write.setPreferredSize(new Dimension(640,290));
+        	Write.setBackground(Color.WHITE);
+        	mainPanel.add(Write);
+        	Write.setLayout(null);
+        	
+        	JLabel Tester = new JLabel("Test");
+        	Tester.setIcon(new ImageIcon("D:\\Downloads\\icons8-user-avatar-filled-50.png"));
+        	Tester.setBounds(0, 0, 50, 50);
+        	Write.add(Tester);
+        	
+        	textField = new JTextField();
+        	textField.setBounds(50, 50, 580, 190);
+        	Write.add(textField);
+        	textField.setColumns(10);
+        	
+        	JLabel lblNewLabel_1 = new JLabel("%d\uB2D8, \uBB34\uC2A8 \uC0DD\uAC01\uC744 \uD558\uC2DC\uACE0 \uACC4\uC2E0\uAC00\uC694?");
+        	lblNewLabel_1.setBounds(50, 0, 590, 50);
+        	Write.add(lblNewLabel_1);
+        	
+        	JButton btnNewButton = new JButton("\uAC8C\uC2DC");
+        	btnNewButton.setForeground(Color.WHITE);
+        	btnNewButton.setBackground(new Color(66, 103, 178));
+        	btnNewButton.setBounds(10, 250, 620, 30);
+        	Write.add(btnNewButton);
+        	
+        for(int i =0;i<rowCnt;i++){
+        	
+        	JPanel Modeler = new JPanel();
+        	try {
+        		
+        	} catch (Exception e) {
+        		e.printStackTrace();
+        	}
+
+        	
+        	GridBagLayout Modeler_Layout = new GridBagLayout();
+        	Modeler.setBackground(Color.WHITE);
+        	Modeler.setOpaque(true);
+        	Modeler.setBorder(BorderFactory.createCompoundBorder(new MatteBorder(0,0,10,0,new Color(233, 235, 238)),new EtchedBorder()));
+    		Modeler_Layout.columnWidths = new int[]{0, 0, 0};
+    		Modeler_Layout.rowHeights = new int[]{50, 50, 0};
+    		Modeler_Layout.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+    		Modeler_Layout.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+    		Modeler.setLayout(Modeler_Layout);
+    		
+//        	JLabel label = new JLabel("Enter Name " + i );
+    		JLabel label = new JLabel("");
+    		label.setIcon(new ImageIcon("D:\\Downloads\\icons8-user-avatar-filled-50.png"));
+    		GridBagConstraints gbc_label = new GridBagConstraints();
+    		gbc_label.insets = new Insets(0, 0, 5, 5);
+    		gbc_label.gridx = 0;
+    		gbc_label.gridy = 0;
+    		Modeler.add(label, gbc_label);
+    		 
+       		JLabel text = new JLabel(names.get(i));
+       		
+       		
+       		
+    		text.setHorizontalAlignment(JLabel.LEFT);
+       		GridBagConstraints gbc_label_2 = new GridBagConstraints();
+    		gbc_label_2.anchor = GridBagConstraints.WEST;
+    		gbc_label_2.gridx = 1;
+    		gbc_label_2.gridy = 0;
+    		Modeler.add(text, gbc_label_2);
+    		
+    		JLabel text2 = new JLabel("");
+    		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+    		gbc_lblNewLabel_1.insets = new Insets(0, 0, 0, 5);
+    		gbc_lblNewLabel_1.gridx = 0;
+    		gbc_lblNewLabel_1.gridy = 1;
+    		Modeler.add(text2, gbc_lblNewLabel_1);
+
+    		String GetText = contents.get(i);
+    		String[] GetTextArray;
+
+    		int k = 0; // 50자로 자른 줄의 갯수, Initialize
+    		int l = 0; // while문 Count 갯수, Initalize
+    		
+    		String StartText = "", RemainText = ""; // 50자로 나눈 문자열의 내용, 나머지 문자열의 내용
+    		String PerfectText = "";
+    		
+    		if(GetText.length() >= 70) {
+       		StartText = GetText.substring(0,70);
+       		RemainText = GetText.substring(70, GetText.length());
+    		} else {
+    			RemainText = contents.get(i);
+    		}
+    		
+       		PerfectText = StartText + "<br />";
+       		
+       		while(RemainText.length() >= 70) {
+       			PerfectText += StartText + "<br />";
+       			StartText = RemainText.substring(0, 70);
+       			RemainText = RemainText.substring(70, RemainText.length());
+       			k++;
+       		}
+       		
+       		PerfectText += RemainText;
+       		PerfectText = "<html>" + PerfectText + "</html>";
+       		
+       		System.out.println(RemainText.length());
+    		JLabel text3 = new JLabel(PerfectText);
+            
+            
+            GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+    		gbc_lblNewLabel_3.anchor = GridBagConstraints.NORTHWEST;
+    		gbc_lblNewLabel_3.gridx = 1;
+    		gbc_lblNewLabel_3.gridy = 1;
+    		Modeler.add(text3, gbc_lblNewLabel_3);
+
+      
+//        layout.putConstraint(SpringLayout.WEST, Modeler, 10, SpringLayout.WEST,
+//                        contentPane);
+      layout.putConstraint(SpringLayout.NORTH, Modeler, j, SpringLayout.NORTH,
+                       contentPane);
+//        layout.putConstraint(SpringLayout.NORTH, Modeler, j, SpringLayout.NORTH,
+//                        contentPane);
+//        layout.putConstraint(SpringLayout.WEST, Modeler, 20, SpringLayout.EAST,
+//                        label);
+        j+=150+k*10;
+        initialX+=100;
+        initialHeight+=100;
+        mainPanel.add(Modeler);
+    	Modeler.setPreferredSize(new Dimension(640, 150+k*10));
+        }
+        frame.getContentPane().setLayout(null);
+        mainPanel.setPreferredSize(new Dimension(mainPanel.getWidth(), j));
+        scroll.setPreferredSize(new Dimension(100,100));
+        scroll.setViewportView(mainPanel);
+        scroll.setBackground(new Color(233, 235, 238));
+        scroll.getVerticalScrollBar().setUnitIncrement(16);
+        scroll.setBorder(BorderFactory.createEmptyBorder());
+
+        JScrollBar vertical = scroll.getVerticalScrollBar();
+        vertical.setPreferredSize(new Dimension(0,0));
+
+        
+        contentPane.add(scroll);
+        mainPanel.setBackground(new Color(233, 235, 238));
+        
+        JPanel panel = new JPanel();
+        panel.setBorder(null);
+        panel.setBounds(0, 0, 1440, 80);
+        frame.getContentPane().add(panel);
+        panel.setLayout(null);
+        panel.setBackground(new Color(72,103,170));
+        JLabel lblNewLabel = new JLabel("");
+        lblNewLabel.setIcon(new ImageIcon("D:\\Downloads\\icons8-facebook-48.png"));
+        lblNewLabel.setBounds(110, 12, 56, 56);
+        panel.add(lblNewLabel);
+        Border emptyBorder = BorderFactory.createEmptyBorder();
+        
+        textField_1 = new JTextField();
+        textField_1.setBounds(184, 24, 600, 34);
+        btnNewButton.setBorder(emptyBorder);
+
+        panel.add(textField_1);
+        textField_1.setColumns(10);
+        
+
+        
+        JButton btnNewButton_1 = new JButton("");
+        btnNewButton_1.setIcon(new ImageIcon("D:\\Downloads\\icons8-search-25.png"));
+        btnNewButton_1.setBackground(new Color(246, 247, 249));
+        btnNewButton.setBorder(emptyBorder);
+        btnNewButton.setContentAreaFilled(false);
+        btnNewButton_1.setBounds(784, 24, 105, 34);
+        panel.add(btnNewButton_1);
+        
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(110, 100, 280, 900);
+        scrollPane.setBackground(Color.WHITE);
+        
+        frame.getContentPane().add(scrollPane);
+        
+        
+        JScrollPane scrollPane_1 = new JScrollPane();
+        scrollPane_1.setBounds(1050, 100, 280, 900);
+        scrollPane_1.setBackground(Color.WHITE);
+        frame.getContentPane().add(scrollPane_1);
+        //mainWindow.add(contentPane);
+        frame.setSize(1440, 1000);
+		frame.setUndecorated(true);
+        frame.setVisible(true);
+	    }
+	
+	  public void DBConnection() {
+		  try {
+		  Class.forName("com.mysql.jdbc.Driver");
+	        con = DriverManager.getConnection("jdbc:mysql://35.166.92.90:3306/sampledb" , "sampledb_dba", "1234");
+		  } catch (Exception e) {
+			  e.printStackTrace();
+		  }
+	  }
+	}
